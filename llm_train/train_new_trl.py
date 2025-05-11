7d03ef1e9596488c2cd78a91014006317de973f4
JunkRoy <shenroy92@gmail.com>
2025-05-11 11:45:19 +0800

    rollback label in args staff

--- llm_train/train_new_trl.py
+++ llm_train/train_new_trl.py
@@ -113,17 +113,17 @@ class DataCollator(object):
         input_ids_batch, labels_batch = [], []
         for instance in batch:
             input_ids = instance["input_ids"]
-            # labels = instance["labels"]
+            labels = instance["labels"]
 
             padding_len = batch_max_len - len(input_ids)
             input_ids = input_ids + [self.pad_token_id] * padding_len
-            # labels = labels + [-100] * padding_len
+            labels = labels + [-100] * padding_len
 
             input_ids_batch.append(input_ids)
-            # labels_batch.append(labels)
+            labels_batch.append(labels)
 
-        return {"input_ids": torch.tensor(input_ids_batch, dtype=torch.long)}#,
-                # "labels": torch.tensor(labels_batch, dtype=torch.long)}
+        return {"input_ids": torch.tensor(input_ids_batch, dtype=torch.long),
+                "labels": torch.tensor(labels_batch, dtype=torch.long)}
 
 
 # Define and parse arguments.
